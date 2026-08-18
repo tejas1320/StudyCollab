b04e105c54a4873f4bf831902ccb3b8b620a6a1a
TEJAS <tejasnaidu8762@gmail.com>
2026-08-19 01:06:05 +0530

    Fix Railway Flask deployment

--- app.py
+++ app.py
@@ -25,8 +25,9 @@ def home():
 
 
 if __name__ == "__main__":
+    import os
+
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
